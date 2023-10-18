0f2f1db3b5483eca5389c45d636545d0df7d8de3
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-10-18 13:53:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function rot13(Str) {
 	decodedArr.join("");
   return decodedArr; //return decodedArr
 }
-
+rot13(Str);
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(rot13("SERR YBIR? NPPVBWBO"));
