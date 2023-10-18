dbb80e2d5a8b795e365ec494d4aa363979966aae
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-10-18 13:12:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -31,11 +31,14 @@ const lookup = {
   ",": ",",
 };
 
-function rot13(encodedStr) {
+function rot13(Str) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
-  return; //return decodedArr
+      str.forEach((word,index)=>{
+		  decodedArr[index]=lookup[word.index];
+	  })
+	decodedArr.join("");
+  return decodedArr; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
